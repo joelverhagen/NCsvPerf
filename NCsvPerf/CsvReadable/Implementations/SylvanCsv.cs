3b87fd58c2103a74b39ca23881608122a759811c
MarkPflug <mark.pflug@live.com>
2021-01-04 14:05:09 -0800

    enable string pooling to absolutely crush the competition.
    This might be considered "unfair" as it takes advantage of the construction of the test data. However, real-life datasets have shown significant savings here too.

--- NCsvPerf/CsvReadable/Implementations/SylvanCsv.cs
+++ NCsvPerf/CsvReadable/Implementations/SylvanCsv.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using Sylvan;
 using Sylvan.Data.Csv;
 
 namespace Knapcode.NCsvPerf.CsvReadable
 {
     /// <summary>
-    /// Package: https://www.nuget.org/packages/NReco.Csv/
-    /// Source: https://github.com/nreco/csv
+    /// Package: https://www.nuget.org/packages/Sylvan.Data.Csv/
+    /// Source: https://github.com/MarkPflug/Sylvan
     /// </summary>
     public class SylvanCsv : ICsvReader
     {
@@ -21,10 +22,18 @@ namespace Knapcode.NCsvPerf.CsvReadable
         {
             var activate = ActivatorFactory.Create<T>(_activationMethod);
             var allRecords = new List<T>();
+            // 64 should fully cover the values in the dataset.
+            var stringPool = new StringPool(64); 
 
             using (var reader = new StreamReader(stream))
             {
-                var csvReader = CsvDataReader.Create(reader, new CsvDataReaderOptions() { HasHeaders = false, BufferSize = 0x10000 });
+                var opts = new CsvDataReaderOptions() {
+                    HasHeaders = false,
+                    BufferSize = 0x10000,
+                    StringFactory = stringPool.GetString
+                };
+
+                var csvReader = CsvDataReader.Create(reader, opts);
                 while (csvReader.Read())
                 {
                     var record = activate();
