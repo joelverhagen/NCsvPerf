d201f24fc473fad283141523a50c3c3d82d74f6f
Joel Verhagen <joel.verhagen@gmail.com>
2023-03-17 14:22:56 -0400

    Remove unnecessary usings

--- NCsvPerf/CsvReadable/Implementations/StackOverflowRegex.cs
+++ NCsvPerf/CsvReadable/Implementations/StackOverflowRegex.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using Knapcode.NCsvPerf.HomeGrown;
 
 namespace Knapcode.NCsvPerf.CsvReadable
 {
