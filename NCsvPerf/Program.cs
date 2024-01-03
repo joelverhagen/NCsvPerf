40dd13d73dba46ec74f0893bcca34ab38417403c
ntr <ntr@ihfood.dk>
2024-01-03 14:22:52 +0100

    Add bench.ps1 and customize run and exporters

--- NCsvPerf/Program.cs
+++ NCsvPerf/Program.cs
@@ -1,4 +1,9 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using Knapcode.NCsvPerf.CsvReadable.TestCases;
 
@@ -8,11 +13,27 @@ namespace Knapcode.NCsvPerf
     {
         private static void Main(string[] args)
         {
-            IConfig config = null;
 #if DEBUG
-            config = new DebugInProcessConfig();
+            var config = new DebugInProcessConfig();
 #else
-            config = null;
+            // Remove time units from column values for csv exporter as per:
+            // https://github.com/dotnet/BenchmarkDotNet/issues/1207
+            var csvExporter = new CsvExporter(
+                CsvSeparator.CurrentCulture,
+                new SummaryStyle(
+                    cultureInfo: System.Globalization.CultureInfo.InvariantCulture,
+                    printUnitsInHeader: true,
+                    printUnitsInContent: false,
+                    timeUnit: Perfolizer.Horology.TimeUnit.Millisecond,
+                    sizeUnit: SizeUnit.KB
+                ));
+            var config = ManualConfig
+                .CreateEmpty()
+                .AddColumnProvider(DefaultColumnProviders.Instance)
+                .AddLogger(ConsoleLogger.Default)
+                .AddExporter(csvExporter)
+                .AddExporter(HtmlExporter.Default)
+                .AddExporter(MarkdownExporter.GitHub);
 #endif
             BenchmarkRunner.Run<PackageAssetsSuite>(config, args);
         }
