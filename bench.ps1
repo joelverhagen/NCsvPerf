30f199e4858e967faacb91c23d2d7d023457ea94
Joel Verhagen <joel.verhagen@gmail.com>
2024-01-12 17:59:45 -0500

    Enhance bench.ps1

--- bench.ps1
+++ bench.ps1
@@ -1,3 +1,15 @@
 #!/usr/bin/env pwsh
 # Add `--filter *.METHOD*` or similar to run subset of benchmarks
-dotnet run -c Release --project NCsvPerf\NCsvPerf.csproj -- -m --warmupCount 1 --minIterationCount 2 --maxIterationCount 5 --iterationTime 2000
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$Filter = "*",
+
+    [Parameter(Mandatory = $false)]
+    [switch]$UseServerGC
+)
+
+$project = Join-Path $PSScriptRoot "NCsvPerf/NCsvPerf.csproj"
+dotnet build $project -c Release "/p:ServerGarbageCollection=$UseServerGC"
+dotnet run --project $project -c Release --no-build -- -m --warmupCount 1 --minIterationCount 2 --maxIterationCount 5 --iterationTime 2000 --filter $Filter
